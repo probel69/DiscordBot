b352028b32e3cafaba7aaeaabcc59f50e176eae2
Mike Ash <mike.ash@semcon.com>
2021-03-05 08:17:19 +0100

    Initial commit

---
+++ src/ProBot/Commands/UsefulCommands.cs
@@ -0,0 +1,64 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using ProBot.Attributes;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProBot.Commands
+{
+    public class UsefulCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Ping Pong")]
+        public async Task Ping(CommandContext ctx)
+        {
+            // Simple Ping Pong message
+            await ctx.Channel.SendMessageAsync("Pong").ConfigureAwait(false);
+        }
+
+        [Command("poll")]
+        [Description("Creates a poll based on Title, Duration and Reactions")]
+        [RequireCategoriesAtrribute(ChannelCheckMode.Any,"CHAT")]
+        public async Task Poll(CommandContext ctx,
+            [Description("Poll's duration i.e 10s 5m 2h...")] TimeSpan duration,
+            [Description("Title of the poll")] string pollTitle,
+            [Description("Poll's reactions to be used as result")] params DiscordEmoji[] emojiOptions)
+        {
+            // Create interactivity extension
+            var interactivity = ctx.Client.GetInteractivity();
+            // Get the Emojis as strings
+            var options = emojiOptions.Select(x => x.ToString());
+            // Get user avatar 
+            var avatar = ctx.User.GetAvatarUrl(DSharpPlus.ImageFormat.Png);
+
+            // Create the Embed
+            var pollEmbed = new DiscordEmbedBuilder();
+            pollEmbed.WithTitle($"Poll for : {pollTitle}"); //Title
+            pollEmbed.WithDescription(string.Join("   ", options)); //Description
+            pollEmbed.WithColor(DiscordColor.Azure); //Form color
+            pollEmbed.WithFooter($"{ctx.User.Username}", avatar ?? ctx.User.DefaultAvatarUrl); // Form footer
+            pollEmbed.WithThumbnail(avatar, 10, 10); // Thumbnail
+            var pollMessage = await ctx.Channel.SendMessageAsync(embed: pollEmbed).ConfigureAwait(false); // Send the embed to the channel
+
+            // Loop through different emojis that were passed in
+            foreach (var option in emojiOptions)
+            {
+                await pollMessage.CreateReactionAsync(option).ConfigureAwait(false);
+            }
+
+            // Get reactions for the specified time duration
+            var pollResult = await interactivity.CollectReactionsAsync(pollMessage, duration).ConfigureAwait(false);
+            // Make a distinct list by excluding bot pre reaction
+            var distinctResult = pollResult.Distinct();
+            // Gather total for each pressed emojis
+            var finalResult = distinctResult.Select(x => $"{x.Emoji}: {x.Total} \n");
+            // Format the message output
+            await ctx.Channel.SendMessageAsync("Final Result: \n ------------ \n");
+            // Send back the result to the channel
+            await ctx.Channel.SendMessageAsync(string.Join("\n", finalResult)).ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/ProBot/ProBot.cs
@@ -0,0 +1,161 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using DSharpPlus.VoiceNext;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ProBot.Commands;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBot
+{
+    public class ProBot
+    {
+        public readonly EventId BotEventId = new EventId(42, "Bot-Ex04");
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+        public InteractivityConfiguration Interactivity { get; set; }
+        public VoiceNextExtension Voice { get; set; }
+
+        public async Task RunAsync()
+        {
+            string json;
+
+            // Read the config.json file
+            await using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            // Deserialize
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            // Pass in the token to the config
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+                //UseInternalLogHandler = true
+            };
+
+            // Create the client with the config
+            Client = new DiscordClient(config);
+
+            // Add event handlers
+            Client.Ready += OnClientReady;
+            Client.GuildAvailable += Client_GuildAvailable;
+            Client.ClientErrored += Client_ClientError;
+
+            // Create an instance of interactivity extension 
+            Client.UseInteractivity(new InteractivityConfiguration
+            {
+                Timeout = TimeSpan.FromMinutes(2)
+            });
+
+            // Create the commandNext config
+            var commandConfigs = new CommandsNextConfiguration
+            {
+                StringPrefixes = new[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true
+
+            };
+
+            // Configure command next module
+            Commands = Client.UseCommandsNext(commandConfigs);
+
+            Commands.CommandExecuted += Commands_CommandExecuted;
+            Commands.CommandErrored += Commands_CommandThrewError;
+
+            // Register commands
+            Commands.RegisterCommands<UsefulCommands>();
+            Commands.RegisterCommands<VoiceCommands>();
+
+            // Enable voice
+            Voice = Client.UseVoiceNext();
+
+            // Connect
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient sender, ReadyEventArgs e)
+        {
+            // Log the fact that this event occurred
+            sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+            return Task.CompletedTask;
+        }
+
+        private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            // Log the name of the server that was just
+            // sent to our client
+            sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+            // Since this method is not async, it shall return
+            // a completed task, so that no more additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private Task Commands_CommandExecuted(CommandsNextExtension sender, CommandExecutionEventArgs e)
+        {
+            // Log the name of the command and user
+            e.Context.Client.Logger.LogInformation(BotEventId, $"{e.Context.User.Username} successfully executed '{e.Command.QualifiedName}'");
+
+            // Since this method is not async, it shall return
+            // a completed task, so that no more additional work
+            // is done
+            return Task.CompletedTask;
+        }
+
+        private async Task Commands_CommandThrewError(CommandsNextExtension sender, CommandErrorEventArgs e)
+        {
+            // Log the error details
+            e.Context.Client.Logger.LogError(BotEventId,
+                $"{e.Context.User.Username} tried executing '{e.Command?.QualifiedName ?? "<unknown command>"}' but it threw error: {e.Exception.GetType()}: {e.Exception.Message}",
+                DateTime.Now);
+
+            // Check if the error is a due to lack
+            // of required permissions
+            if (e.Exception is ChecksFailedException)
+            {
+                // If yes, the user doesn't have the required permissions, 
+                // Notify
+                var emojis = DiscordEmoji.FromName(e.Context.Client, ":no_entry:");
+
+                // Wrap the response into an embed
+                var embed = new DiscordEmbedBuilder
+                {
+                    Title = "Access denied",
+                    Description = $"{emojis} You do not have the permissions required to execute this command.",
+                    Color = new DiscordColor(0xFF0000) // Red
+                };
+                await e.Context.RespondAsync(embed);
+            }
+        }
+
+        private Task Client_ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            // Log the details of the error that just 
+            // occurred in our client
+            sender.Logger.LogError(BotEventId, e.Exception, "Exception occurred");
+
+            // Since this method is not async, it shall return
+            // a completed task, so that no more additional work
+            // is done
+            return Task.CompletedTask;
+        }
+    }
+}
+
